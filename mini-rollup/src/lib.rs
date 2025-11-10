f5346a84f99ae0bdad81b663c61d30377a908537
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-11-10 14:21:54 -0800

    StateChannel struct

--- mini-rollup/src/lib.rs
+++ mini-rollup/src/lib.rs
@@ -1,14 +1,25 @@
+/* mod loader;
+mod processor;
+mod settler; */
+pub mod transaction;
+
 use {
-    
     solana_client::rpc_client::RpcClient,
-   
-    solana_svm::transaction_processor::{
+    solana_sdk::signature::Keypair,
+  /*   solana_svm::transaction_processor::{
         TransactionProcessingConfig, TransactionProcessingEnvironment,
-    },
+    }, */
     
 };
 
 pub struct StateChannel {
-
+    /// I think you know why this is a bad idea...
+    keys: Vec<Keypair>,
+    rpc_client: RpcClient,
 }
 
+impl StateChannel {
+    pub fn new(keys: Vec<Keypair>, rpc_client: RpcClient) -> Self {
+        Self { keys, rpc_client }
+    }
+}
