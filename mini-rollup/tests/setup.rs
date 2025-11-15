d4a640753131ef9564890d1871affd4f6c9c8690
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-11-15 10:47:01 -0800

    remove commented code

--- mini-rollup/tests/setup.rs
+++ mini-rollup/tests/setup.rs
@@ -10,7 +10,6 @@ use {
         system_program,
     },
     solana_test_validator::{TestValidator, TestValidatorGenesis},
-  //  spl_token::state::{Account as TokenAccount, Mint},
 };
 
 const SLOTS_PER_EPOCH: u64 = 50;
@@ -43,52 +42,10 @@ impl TestValidatorContext {
     }
 }
 
-/* pub fn get_token_account_balance(token_account: Account) -> u64 {
-    let state = TokenAccount::unpack(token_account.data()).unwrap();
-    state.amount
-} */
 
-/* pub fn mint_account() -> AccountSharedData {
-    let data = {
-        let mut data = [0; Mint::LEN];
-        Mint::pack(
-            Mint {
-                supply: 100_000_000,
-                decimals: 0,
-                is_initialized: true,
-                ..Default::default()
-            },
-            &mut data,
-        )
-        .unwrap();
-        data
-    };
-    let mut account = AccountSharedData::new(100_000_000, data.len(), &spl_token::id());
-    account.set_data_from_slice(&data);
-    account
-} */
 
 pub fn system_account(lamports: u64) -> AccountSharedData {
     AccountSharedData::new(lamports, 0, &system_program::id())
 }
 
-/* pub fn token_account(owner: &Pubkey, mint: &Pubkey, amount: u64) -> AccountSharedData {
-    let data = {
-        let mut data = [0; TokenAccount::LEN];
-        TokenAccount::pack(
-            TokenAccount {
-                mint: *mint,
-                owner: *owner,
-                amount,
-                state: spl_token::state::AccountState::Initialized,
-                ..Default::default()
-            },
-            &mut data,
-        )
-        .unwrap();
-        data
-    };
-    let mut account = AccountSharedData::new(100_000_000, data.len(), &spl_token::id());
-    account.set_data_from_slice(&data);
-    account
-} */
+
