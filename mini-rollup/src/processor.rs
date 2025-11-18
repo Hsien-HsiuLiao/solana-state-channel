f4f15444ff69938ae31e660957ce0f709e4a758d
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-11-17 20:31:03 -0800

    remove unneeded code

--- mini-rollup/src/processor.rs
+++ mini-rollup/src/processor.rs
@@ -6,7 +6,7 @@ use {
     solana_program_runtime::loaded_programs::{
         BlockRelation, ForkGraph, LoadProgramMetrics, ProgramCacheEntry,
     },
-    solana_sdk::{account::ReadableAccount, clock::Slot, feature_set::FeatureSet, transaction},
+    solana_sdk::{clock::Slot, feature_set::FeatureSet, transaction},
     solana_svm::{
         account_loader::CheckedTransactionDetails,
         transaction_processing_callback::TransactionProcessingCallback,
@@ -21,16 +21,16 @@ use {
 /// processor how to work across forks.
 ///
 /// Since PayTube doesn't use slots or forks, this implementation is mocked.
-pub(crate) struct PayTubeForkGraph {}
+pub(crate) struct StateChannelForkGraph {}
 
-impl ForkGraph for PayTubeForkGraph {
+impl ForkGraph for StateChannelForkGraph {
     fn relationship(&self, _a: Slot, _b: Slot) -> BlockRelation {
         BlockRelation::Unknown
     }
 }
 
 /// This function encapsulates some initial setup required to tweak the
-/// `TransactionBatchProcessor` for use within PayTube.
+/// `TransactionBatchProcessor` 
 ///
 /// We're simply configuring the mocked fork graph on the SVM API's program
 /// cache, then adding the System program to the processor's builtins.
@@ -38,14 +38,14 @@ pub(crate) fn create_transaction_batch_processor<CB: TransactionProcessingCallba
     callbacks: &CB,
     feature_set: &FeatureSet,
     compute_budget: &ComputeBudget,
-) -> TransactionBatchProcessor<PayTubeForkGraph> {
-    let processor = TransactionBatchProcessor::<PayTubeForkGraph>::default();
+) -> TransactionBatchProcessor<StateChannelForkGraph> {
+    let processor = TransactionBatchProcessor::<StateChannelForkGraph>::default();
 
     {
         let mut cache = processor.program_cache.write().unwrap();
 
         // Initialize the mocked fork graph.
-        cache.fork_graph = Some(Arc::new(RwLock::new(PayTubeForkGraph {})));
+        cache.fork_graph = Some(Arc::new(RwLock::new(StateChannelForkGraph {})));
 
         // Initialize a proper cache environment.
         // (Use Loader v4 program to initialize runtime v2 if desired)
@@ -54,26 +54,8 @@ pub(crate) fn create_transaction_batch_processor<CB: TransactionProcessingCallba
                 .unwrap(),
         );
 
-        // Add the SPL Token program to the cache.
-       /*  if let Some(program_account) = callbacks.get_account_shared_data(&spl_token::id()) {
-            let elf_bytes = program_account.data();
-            let program_runtime_environment = cache.environments.program_runtime_v1.clone();
-            cache.assign_program(
-                spl_token::id(),
-                Arc::new(
-                    ProgramCacheEntry::new(
-                        &solana_sdk::bpf_loader::id(),
-                        program_runtime_environment,
-                        0,
-                        0,
-                        elf_bytes,
-                        elf_bytes.len(),
-                        &mut LoadProgramMetrics::default(),
-                    )
-                    .unwrap(),
-                ),
-            );
-        } */
+        // Can add program to the cache here if needed.
+      
     }
 
     // Add the system program builtin.
@@ -88,17 +70,8 @@ pub(crate) fn create_transaction_batch_processor<CB: TransactionProcessingCallba
         ),
     );
 
-    // Add the BPF Loader v2 builtin, for the SPL Token program.
-    processor.add_builtin(
-        callbacks,
-        solana_sdk::bpf_loader::id(),
-        "solana_bpf_loader_program",
-        ProgramCacheEntry::new_builtin(
-            0,
-            b"solana_bpf_loader_program".len(),
-            solana_bpf_loader_program::Entrypoint::vm,
-        ),
-    );
+    // Can add another builtin here if needed, for example BPF loader for SPL token program
+   
 
     processor
 }
